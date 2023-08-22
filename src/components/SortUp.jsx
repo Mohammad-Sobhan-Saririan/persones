808a1a437f96a400f826be2d5d35a4a12b15011c
Mohammad-Sobhan-Saririan <2429sobhan@gmail.com>
2023-08-22 18:40:02 +0330

    Delete SortUp.jsx
    delete unused files

--- src/components/SortUp.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class SortUp extends Component {
-    state = {};
-    render() {
-        return <i className="fa fa-sort-up" aria-hidden="true"></i>;
-    }
-}
-
-export default SortUp;
