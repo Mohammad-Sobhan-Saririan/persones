9789bbbc75df858a9319a756914cebef4eb9e2cf
M.Sobhan.Saririan <2429sobhan@gmail.com>
2023-08-22 18:18:55 +0330

    common files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import Persones from "./components/Persones/Persones";
+import { Route, Routes, Navigate } from "react-router-dom";
+import PersonForm from "./components/PersonForm";
+import NotFound from "./components/NotFound";
+import Coustomers from "./components/Coustomers";
+import LoginForm from "./components/Login/LoginForm";
+import NavBar from "./components/Navbar/Navbar";
+import { getPersones } from "./services/FakePersoneServices";
+import {AltLoginForm} from "./components/Login/LoginForm";
+import Update from "./components/Update/Update";
+import {AltUpdateForm} from "./components/Update/Update";
+class App extends React.Component {
+    state = {
+        persones: getPersones(),
+    };
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar
+                    persones={this.state.persones}
+                    onChange={this.handleChange}
+                    search={this.state.search}
+                />
+                <main className="container">
+                    <Routes>
+                        <Route path="/Update/:id/:name/:lastname/:city/:email/:mobilephone" element={<AltUpdateForm/>}></Route>
+                        <Route
+                            path="/Persones"
+                            // Component={<Persones persones={this.state.persones} />}
+                            element={
+                                <Persones persones={this.state.persones} />
+                            }
+                            render 
+                        ></Route>
+                        <Route
+                            path="/Person-Form"
+                            element={<PersonForm />}
+                            // Component={<PersonForm />}
+                        ></Route>
+                        <Route path="/not-found" element={<NotFound />}></Route>
+                        <Route
+                            path="/Coustomers"
+                            // Component={<Coustomers />}
+                            element={<Coustomers />}
+                        ></Route>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/Persones" />}
+                            // Component={<Navigate to="/Persones" />}
+                        ></Route>
+                        {/* <Route
+                            path="*"
+                            element={<Navigate to="/not-found" />}
+                        ></Route> */}
+                        <Route path="/Login" element={<AltLoginForm/>}></Route>
+                        <Route path='/Persones/:id' element={<PersonForm/>}></Route>
+                    </Routes>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css"; // import bootstrap
+import "font-awesome/css/font-awesome.css"; // import fontawesome
+import "./fontawesome-free-6.4.2-web/css/all.css"; // import fontawesome
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div
+        data-bs-spy="scroll"
+        data-bs-target="#navbar-example2"
+        data-bs-offset="0"
+        tabIndex="0"
+        data-bs-toggle="scrollspy"
+        className="scrollspy-example m-0 p-0"
+    >
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
