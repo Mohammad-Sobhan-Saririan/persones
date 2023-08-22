b63aac16c7ab2ba80f1b95ca959b6adcb7058499
Mohammad-Sobhan-Saririan <2429sobhan@gmail.com>
2023-08-22 18:39:18 +0330

    Delete SortDown.jsx
    delete unused files

--- src/components/SortDown.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class SortDown extends Component {
-    state = {};
-    render() {
-        return <i className="fa fa-sort-down" aria-hidden="true"></i>;
-    }
-}
-
-export default SortDown;
